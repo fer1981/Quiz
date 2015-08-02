90d71b9ef9fe781205991ebf67fbcd4b8866b11d
fer1981 <femparis@gmail.com>
2015-08-02 14:49:53 +0200

    Borrar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -100,3 +100,10 @@ req.quiz
     }
   );
 };
+
+// DELETE /quizes/:id
+exports.destroy = function(req, res)  {
+  req.quiz.destroy().then(  function()  {
+    res.redirect('/quizes');
+  }).catch(function(error){next(error)});
+};
